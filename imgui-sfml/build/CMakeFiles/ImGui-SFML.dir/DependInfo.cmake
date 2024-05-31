c894877b351a13622a84965ea75bcbfd4d8c85c5
Hamza Ihsan <thehamzaihsan@gmail.com>
2024-05-31 16:08:34 +0500

    added libs

---
+++ imgui-sfml/build/CMakeFiles/ImGui-SFML.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/hamzaihsan/projects/sfml/imgui-engine-base/imgui/imgui.cpp" "CMakeFiles/ImGui-SFML.dir/home/hamzaihsan/projects/sfml/imgui-engine-base/imgui/imgui.cpp.o" "gcc" "CMakeFiles/ImGui-SFML.dir/home/hamzaihsan/projects/sfml/imgui-engine-base/imgui/imgui.cpp.o.d"
+  "/home/hamzaihsan/projects/sfml/imgui-engine-base/imgui/imgui_demo.cpp" "CMakeFiles/ImGui-SFML.dir/home/hamzaihsan/projects/sfml/imgui-engine-base/imgui/imgui_demo.cpp.o" "gcc" "CMakeFiles/ImGui-SFML.dir/home/hamzaihsan/projects/sfml/imgui-engine-base/imgui/imgui_demo.cpp.o.d"
+  "/home/hamzaihsan/projects/sfml/imgui-engine-base/imgui/imgui_draw.cpp" "CMakeFiles/ImGui-SFML.dir/home/hamzaihsan/projects/sfml/imgui-engine-base/imgui/imgui_draw.cpp.o" "gcc" "CMakeFiles/ImGui-SFML.dir/home/hamzaihsan/projects/sfml/imgui-engine-base/imgui/imgui_draw.cpp.o.d"
+  "/home/hamzaihsan/projects/sfml/imgui-engine-base/imgui/imgui_tables.cpp" "CMakeFiles/ImGui-SFML.dir/home/hamzaihsan/projects/sfml/imgui-engine-base/imgui/imgui_tables.cpp.o" "gcc" "CMakeFiles/ImGui-SFML.dir/home/hamzaihsan/projects/sfml/imgui-engine-base/imgui/imgui_tables.cpp.o.d"
+  "/home/hamzaihsan/projects/sfml/imgui-engine-base/imgui/imgui_widgets.cpp" "CMakeFiles/ImGui-SFML.dir/home/hamzaihsan/projects/sfml/imgui-engine-base/imgui/imgui_widgets.cpp.o" "gcc" "CMakeFiles/ImGui-SFML.dir/home/hamzaihsan/projects/sfml/imgui-engine-base/imgui/imgui_widgets.cpp.o.d"
+  "/home/hamzaihsan/projects/sfml/imgui-engine-base/imgui/misc/cpp/imgui_stdlib.cpp" "CMakeFiles/ImGui-SFML.dir/home/hamzaihsan/projects/sfml/imgui-engine-base/imgui/misc/cpp/imgui_stdlib.cpp.o" "gcc" "CMakeFiles/ImGui-SFML.dir/home/hamzaihsan/projects/sfml/imgui-engine-base/imgui/misc/cpp/imgui_stdlib.cpp.o.d"
+  "/home/hamzaihsan/projects/sfml/imgui-engine-base/imgui-sfml/imgui-SFML.cpp" "CMakeFiles/ImGui-SFML.dir/imgui-SFML.cpp.o" "gcc" "CMakeFiles/ImGui-SFML.dir/imgui-SFML.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
